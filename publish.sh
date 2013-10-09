98f5491dc35411df3ee81ba6e2b63ac60496a01a
jhermann <jh@web.de>
2013-10-09 14:05:10 +0200

    publish with main repo author; check for proper Sphinx install

--- publish.sh
+++ publish.sh
@@ -2,7 +2,19 @@
 set -e
 rootdir=$(cd $(dirname "$0") && pwd)
 workdir="$rootdir/gh-pages"
-origin=$(git config --get remote.origin.url)
+origin=$(git config --get "remote.origin.url")
+user_name=$(git config --get "user.name")
+user_email=$(git config --get "user.email")
+
+fail() {
+    echo "FATAL ERROR:" "$@"
+    exit 1
+}
+
+set_user() {
+    git config --local --replace-all "user.name" "$user_name"
+    git config --local --replace-all "user.email" "$user_email"
+}
 
 init() {
     rm -rf $workdir
@@ -10,9 +22,10 @@ init() {
     pushd $workdir
     git checkout --orphan gh-pages
     git rm -rf .
+    set_user
     echo >.nojekyll "Sphinx generated"
     git add .nojekyll
-    git commit -a -m 'Initialized by paver publish_init'
+    git commit -a -m 'Initialized by publish init'
     git push origin gh-pages
     popd
 }
@@ -29,6 +42,7 @@ publish() {
     pushd $workdir
 
     git init
+    set_user
     git remote add -t gh-pages -f origin "$origin"
     git checkout gh-pages
 
@@ -40,9 +54,9 @@ publish() {
     git commit -m 'Docs published'
     git push origin gh-pages
     git status
-    
+
     popd
 }
 
+which sphinx-build >/dev/null || fail "'sphinx-build' not found, need to activate a virtualenv?"
 test "$1" == "init" && init || publish
-
